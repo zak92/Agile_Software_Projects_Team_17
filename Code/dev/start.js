7aa50d6eb817fc3e252f4fd8f6e7ae1c2813fa32
Zakiyyah Ahmed <zaktech269@gmail.com>
2021-07-19 21:46:01 +0200

    start.js page

--- Code/dev/start.js
+++ Code/dev/start.js
@@ -4,8 +4,8 @@ const mysql = require("mysql2");
 
 //establish db connection
 const db = mysql.createConnection ({ host: "localhost",
-  user: "root",
-  password: "Z@mysqluser13749", //insert password
+  user: "test",
+  password: "newpassword", //insert password
   database: "coding_knowledge_db" }); // insert database name
   db.connect((err) => {
   if (err) {
