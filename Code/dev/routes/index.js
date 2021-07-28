78fff163f66bcc6cb86f2581960c6f839ad1a503
shujun-lee <shujunn.lee@gmail.com>
2021-07-29 02:51:01 +0800

    Update index.js and add new routes for user authentication

--- Code/dev/routes/index.js
+++ Code/dev/routes/index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 
 const router = express.Router();
+var db = require('../db');
 
 router.get("/",function(req, res){
   res.render("index", {
     title: "Site Title",
+    user: req.user 
   });
 });
 
 router.get("/javascript",function(req, res){
   res.render("javascript", {
     title: "Javascript Snippets",
+    user: req.user 
   });
 });
 
@@ -19,16 +22,19 @@ router.get("/search",function(req, res){
 
   let searchTerm = req.query.search 
   // Search db for code snippets based on search query
-  db.query('SELECT * from languages WHERE title like ?', '%' + searchTerm + '%', function (error, results, fields) {
+  db.get('SELECT * from languages WHERE title like ?', '%' + searchTerm + '%', function (error, results, fields) {
     if (error) {
-        res.render("progress");
+        res.render("progress", {
+          user: req.user
+        });
     }
 
     if (results.length <= 0) {
         res.render("search", {
           title: "Search",
           message: 'No results found',
-          results: []
+          results: [],
+          user: req.user
         });
         return;
     }
@@ -40,7 +46,8 @@ router.get("/search",function(req, res){
     res.render("search", {
       title: "Search",
       message: false,
-      results: data
+      results: data,
+      user: req.user 
     });
   });
 });
@@ -49,9 +56,11 @@ router.get("/ranking",function(req,res){
 
   let sql1 = 'SELECT  (@rank:= @rank + 1) AS rank_no, a.* FROM (SELECT username, contribution, upvotes, ROUND(upvotes*0.5 + contribution*0.5,0) AS score FROM (SELECT fk_user_id , SUM(upvotes) AS upvotes, COUNT(*) AS contribution FROM (SELECT * FROM frameworks UNION ALL SELECT * FROM tools UNION ALL SELECT * FROM languages) AS records GROUP BY fk_user_id) AS total LEFT JOIN users ON user_id = fk_user_id ORDER BY score DESC) AS a , (SELECT @rank:= 0) AS b LIMIT 5'
 
-  db.query(sql1,function(error,results){
+  db.get(sql1,function(error,results){
     if(error){
-      res.render("progress");
+      res.render("progress", {
+        user: req.user
+      });
       return;
     }
 
@@ -76,11 +85,11 @@ router.get("/ranking",function(req,res){
   });
 });
 
-
-// --------------------------------------------------------------------------------------------------
-// Note this should be the last endpoint in this file. Otherwise it will override everything after it.
-router.get("*", function(req, res){
-  res.render("progress");
+router.get("/progress", function(req, res){
+  res.render("progress",{
+    user: req.user
+  });
 });
+
 
 module.exports = router;
