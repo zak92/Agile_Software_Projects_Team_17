1d23dbe9fc52671018822ce9ef20a834bf1fdad8
Alexeia <alexeiaw@gmail.com>
2021-08-01 18:13:46 +0200

    move search logic to a module for easier understanding

--- Code/dev/routes/index.js
+++ Code/dev/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 var db = require('../db');
+var mainSearch = require('../modules/search');
 
 router.get("/",function(req, res){
 
@@ -49,102 +50,8 @@ router.get("/javascript",function(req, res){
 });
 
 
-router.get("/search", function(req, res){
-
-  let searchTermMain = req.query.search 
-
-  // Probably not the best way, but this main method synchronously searches multiple tables in the database and then returns 
-  // the results. The use of async/await is to get sqlite3 to be synchronous instead of asynchronous.
-  async function main() {
-    async function languages(searchTerm){
-      let language = `language LIKE '%${searchTerm}%'`
-      let title = `title LIKE '%${searchTerm}%'`
-      let description = `description LIKE '%${searchTerm}%'`
-      let tags = `tags LIKE '%${searchTerm}%'`
-    
-      let sql = `SELECT * FROM languages WHERE ${language} OR ${title} OR ${description} OR ${tags}  ORDER BY language`;
-    
-      return new Promise(function(resolve,reject){
-          db.all(sql, function(err,rows){
-            if(err){return reject(err);}
-            resolve(rows);
-          });
-      });
-    }
-
-    async function frameworks(searchTerm){
-      let framework = `framework LIKE '%${searchTerm}%'`
-      let title = `title LIKE '%${searchTerm}%'`
-      let description = `description LIKE '%${searchTerm}%'`
-      let tags = `tags LIKE '%${searchTerm}%'`
-    
-      let sql = `SELECT * FROM frameworks WHERE ${framework} OR ${title} OR ${description} OR ${tags}  ORDER BY framework`;
-    
-      return new Promise(function(resolve,reject){
-          db.all(sql, function(err,rows){
-            if(err){return reject(err);}
-            resolve(rows);
-          });
-      });
-    }
-
-    async function tools(searchTerm){
-      let framework = `tool LIKE '%${searchTerm}%'`
-      let title = `title LIKE '%${searchTerm}%'`
-      let description = `description LIKE '%${searchTerm}%'`
-      let tags = `tags LIKE '%${searchTerm}%'`
-    
-      let sql = `SELECT * FROM tools WHERE ${framework} OR ${title} OR ${description} OR ${tags}  ORDER BY tool`;
-    
-      return new Promise(function(resolve,reject){
-          db.all(sql, function(err,rows){
-            if(err){return reject(err);}
-            resolve(rows);
-          });
-      });
-    }
-
-    try {
-      let languageResults = await languages(searchTermMain);
-      let frameworksResults = await frameworks(searchTermMain);
-      let toolsResults = await tools(searchTermMain);
-      
-      let formattedLanguageResults = languageResults.map(result => {
-        return Object.values(result)
-      })
-
-      let formattedFrameworksResults = frameworksResults.map(result => {
-        return Object.values(result)
-      })
-
-      let formattedToolsResults = toolsResults.map(result => {
-        return Object.values(result)
-      })
-
-      res.render("search", {
-        title: "Search",
-        message: false,
-        languages: formattedLanguageResults,
-        frameworks: formattedFrameworksResults,
-        tools: formattedToolsResults,
-        user: req.user
-      });
-
-    } catch (error) {
-      console.log(error);
-        res.render("index", {
-          user: req.user,
-          title:"Site Title",
-          message:"DB ERROR",
-          results:[]
-        });
-    }
-
-}
-
-main()
-
-  
+router.get("/search", function(req, res){ 
+  mainSearch.search(req, res, req.query.search)
 });
 
 router.get("/ranking",function(req,res){
