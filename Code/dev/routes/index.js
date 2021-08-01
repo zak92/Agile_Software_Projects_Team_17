2c894e70d98479a01de8cc2d59b711b947ea2dda
Alexeia <alexeiaw@gmail.com>
2021-08-01 17:48:50 +0200

    implment searh functionality for languages implment searh functionality for frameworks implment searh functionality for tools
    render results for tool, frameworks and tools
    site wide search endpoint

--- Code/dev/routes/index.js
+++ Code/dev/routes/index.js
@@ -49,38 +49,102 @@ router.get("/javascript",function(req, res){
 });
 
 
-router.get("/search",function(req, res){
+router.get("/search", function(req, res){
 
-  let searchTerm = req.query.search 
-  // Search db for code snippets based on search query
-  db.get('SELECT * from languages WHERE title like ?', '%' + searchTerm + '%', function (error, results, fields) {
-    if (error) {
-        res.render("progress", {
-          user: req.user
-        });
+  let searchTermMain = req.query.search 
+
+  // Probably not the best way, but this main method synchronously searches multiple tables in the database and then returns 
+  // the results. The use of async/await is to get sqlite3 to be synchronous instead of asynchronous.
+  async function main() {
+    async function languages(searchTerm){
+      let language = `language LIKE '%${searchTerm}%'`
+      let title = `title LIKE '%${searchTerm}%'`
+      let description = `description LIKE '%${searchTerm}%'`
+      let tags = `tags LIKE '%${searchTerm}%'`
+    
+      let sql = `SELECT * FROM languages WHERE ${language} OR ${title} OR ${description} OR ${tags}  ORDER BY language`;
+    
+      return new Promise(function(resolve,reject){
+          db.all(sql, function(err,rows){
+            if(err){return reject(err);}
+            resolve(rows);
+          });
+      });
     }
 
-    if (results.length <= 0) {
-        res.render("search", {
-          title: "Search",
-          message: 'No results found',
-          results: [],
-          user: req.user
-        });
-        return;
+    async function frameworks(searchTerm){
+      let framework = `framework LIKE '%${searchTerm}%'`
+      let title = `title LIKE '%${searchTerm}%'`
+      let description = `description LIKE '%${searchTerm}%'`
+      let tags = `tags LIKE '%${searchTerm}%'`
+    
+      let sql = `SELECT * FROM frameworks WHERE ${framework} OR ${title} OR ${description} OR ${tags}  ORDER BY framework`;
+    
+      return new Promise(function(resolve,reject){
+          db.all(sql, function(err,rows){
+            if(err){return reject(err);}
+            resolve(rows);
+          });
+      });
     }
 
-    let data = results.map(result => {
+    async function tools(searchTerm){
+      let framework = `tool LIKE '%${searchTerm}%'`
+      let title = `title LIKE '%${searchTerm}%'`
+      let description = `description LIKE '%${searchTerm}%'`
+      let tags = `tags LIKE '%${searchTerm}%'`
+    
+      let sql = `SELECT * FROM tools WHERE ${framework} OR ${title} OR ${description} OR ${tags}  ORDER BY tool`;
+    
+      return new Promise(function(resolve,reject){
+          db.all(sql, function(err,rows){
+            if(err){return reject(err);}
+            resolve(rows);
+          });
+      });
+    }
+
+    try {
+      let languageResults = await languages(searchTermMain);
+      let frameworksResults = await frameworks(searchTermMain);
+      let toolsResults = await tools(searchTermMain);
+      
+      let formattedLanguageResults = languageResults.map(result => {
         return Object.values(result)
-    })
+      })
 
-    res.render("search", {
-      title: "Search",
-      message: false,
-      results: data,
-      user: req.user 
-    });
-  });
+      let formattedFrameworksResults = frameworksResults.map(result => {
+        return Object.values(result)
+      })
+
+      let formattedToolsResults = toolsResults.map(result => {
+        return Object.values(result)
+      })
+
+      res.render("search", {
+        title: "Search",
+        message: false,
+        languages: formattedLanguageResults,
+        frameworks: formattedFrameworksResults,
+        tools: formattedToolsResults,
+        user: req.user
+      });
+
+    } catch (error) {
+      console.log(error);
+        res.render("index", {
+          user: req.user,
+          title:"Site Title",
+          message:"DB ERROR",
+          results:[]
+        });
+    }
+
+}
+
+main()
+
+  
 });
 
 router.get("/ranking",function(req,res){
