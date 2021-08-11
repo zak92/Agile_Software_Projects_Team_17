765bc975dab310b27636253f91615c41ba026607
Felixlo <81540099@qq.com>
2021-08-11 22:15:30 +0800

    Delete old comment code

--- Code/dev/routes/index.js
+++ Code/dev/routes/index.js
@@ -43,41 +43,6 @@ router.get("/",async function(req, res){
       results:[]
     });
   }
-
-  // let sql1 = 'SELECT RANK() OVER( ORDER BY a.score DESC) AS rank, a.* FROM (SELECT username, IFNULL(contribution, 0), IFNULL(upvotes, 0), IFNULL(ROUND(upvotes*0.5 + contribution*0.5,0), 0) AS score FROM users LEFT JOIN (SELECT fk_user_id , SUM(upvotes) AS upvotes, COUNT(*) AS contribution FROM (SELECT * FROM frameworks UNION ALL SELECT * FROM tools UNION ALL SELECT * FROM languages) AS records GROUP BY fk_user_id) AS total ON user_id = fk_user_id ORDER BY score DESC) AS a LIMIT 5;';
-
-  // db.all(sql1,function(error,results){
-  //   if(error){
-  //     res.render("index", {
-  //       user: req.user,
-  //       title:"Site Title",
-  //       message:"DB ERROR",
-  //       results:[]
-  //     });
-  //     return;
-  //   }
-
-  //   if(results.length <= 0) {
-  //     res.render("index",{
-  //       user: req.user,
-  //       title:"Site Title",
-  //       message:"NO DATA",
-  //       results:[]
-  //     });
-  //     return;
-  //   }
-    
-  //   let data = results.map(result => {
-  //     return Object.values(result)
-  //   })
-    
-  //   res.render("index", {
-  //     user: req.user,
-  //     title:"Site Title",
-  //     message:false,
-  //     results:data
-  //   });
-  // });
 });
 
 router.get(["/javascript", "/python", "/c%2B%2B"] ,function(req, res){
